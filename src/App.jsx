13f4b64254c4e1cd50e971289c2d5e8195ebbddd
Flavio Avdulla <a.flavio4366@gmail.com>
2024-11-02 22:49:32 +0100

    web

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
-import React from 'react'
-import Navbar from './Components/Navbar/Navbar'
+import React, { useState } from "react";
+import Navbar from "./Components/Navbar/Navbar";
+import { Route, Routes } from "react-router-dom";
+import "@fortawesome/fontawesome-free/css/all.min.css";
 
 const App = () => {
   return (
-    <div className='app'>
-      <Navbar/>
+    <div className="app">
+      <Navbar />
     </div>
-  )
-}
+  );
+};
 
-export default App
+export default App;
--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -1,12 +1,40 @@
-import React from 'react'
-import './Navbar.css'
+import React, { useState } from "react";
+import "./Navbar.css";
+import { Link } from "react-router-dom";
 
-const Navbar = () => {
+const Navbar = ({ setShowLogin }) => {
+  const [menu, setMenu] = useState("home");
+  const [mobileMenu, setMobileMenu] = useState(false);
+  const [iconColor, setIconColor] = useState(false);
+
+  const toggleMenu = () => {
+    setMobileMenu(!mobileMenu);
+    setIconColor(!iconColor);
+  };
+
   return (
-    <div>
-      
-    </div>
-  )
-}
+    <nav className="navbar">
+      <i
+        className={`fa-solid fa-bars ${iconColor ? "active-icon" : ""}`}
+        alt=""
+        onClick={toggleMenu}
+      ></i>
+      <ul className={`navbar-menu ${mobileMenu ? "open" : "closed"}`}>
+        <li>
+          <a>HOME</a>
+        </li>
+        <li>
+          <a>PROJECTS</a>
+        </li>
+        <li>
+          <a>ABOUT ME</a>
+        </li>
+        <li>
+          <a>CONTACTS</a>
+        </li>
+      </ul>
+    </nav>
+  );
+};
 
-export default Navbar
+export default Navbar;
