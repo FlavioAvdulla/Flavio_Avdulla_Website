2a89272dd69787634ce4c8cb849ba2a2c81ace70
Flavio Avdulla <a.flavio4366@gmail.com>
2024-11-07 22:41:59 +0100

    web

--- src/Pages/Projects/Projects.jsx
+++ src/Pages/Projects/Projects.jsx
@@ -1,10 +1,35 @@
-import React from "react";
-import './Projects.css'
+import React, { useState } from "react";
+import "./Projects.css";
+import { texts } from "../../assets/assets.js";
 
 const Projects = () => {
+  const [expandedProjects, setExpandedProjects] = useState({});
+
+  const toggleShowMore = (id) => {
+    setExpandedProjects((prevState) => ({
+      ...prevState,
+      [id]: !prevState[id],
+    }));
+  };
+
   return (
-    <div>
-      <h1>Projects Page</h1>
+    <div className="projects-container">
+      {texts.map((text) => (
+        <div key={text._id} className="projects">
+          <div className="projects-text-container">
+            <h1>{text.project_title}</h1>
+            <p>
+              {expandedProjects[text._id]
+                ? text.description
+                : `${text.description.substring(0, 70)}...`}
+            </p>
+            <button className="show-more-btn" onClick={() => toggleShowMore(text._id)}>
+              {expandedProjects[text._id] ? "Show Less" : "Show More"}
+            </button>
+          </div>
+          <img src={require(`../../assets/assets/${text.image}.jpg`)} alt={text.project_title} />
+        </div>
+      ))}
     </div>
   );
 };
---
+++ src/assets/assets.js
@@ -0,0 +1,38 @@
+export const texts = [
+    {
+        _id: "1",
+        image: "Polaris_SM",
+        project_title: "Project Title 1",
+        description: "Lorem Ipsum is simply dummy text of the printing and typesetting industry, Lorem Ipsum is simply dummy text of."
+    },
+    {
+        _id: "2",
+        image: "Polaris_SM",
+        project_title: "Project Title 2",
+        description: "Lorem Ipsum is simply dummy text of the printing and typesetting industry, Lorem Ipsum is simply dummy text of."
+    },
+    {
+        _id: "3",
+        image: "Polaris_SM",
+        project_title: "Project Title 3",
+        description: "Lorem Ipsum is simply dummy text of the printing and typesetting industry, Lorem Ipsum is simply dummy text of."
+    },
+    {
+        _id: "4",
+        image: "Polaris_SM",
+        project_title: "Project Title 4",
+        description: "Lorem Ipsum is simply dummy text of the printing and typesetting industry, Lorem Ipsum is simply dummy text of."
+    },
+    {
+        _id: "5",
+        image: "Polaris_SM",
+        project_title: "Project Title 5",
+        description: "Lorem Ipsum is simply dummy text of the printing and typesetting industry, Lorem Ipsum is simply dummy text of."
+    },
+    {
+        _id: "6",
+        image: "Polaris_SM",
+        project_title: "Project Title 6",
+        description: "Lorem Ipsum is simply dummy text of the printing and typesetting industry, Lorem Ipsum is simply dummy text of."
+    }
+];
