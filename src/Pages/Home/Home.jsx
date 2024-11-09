144858fa6fe684a5e49f484f7ea79049b3e14327
Flavio Avdulla <a.flavio4366@gmail.com>
2024-11-09 22:53:36 +0100

    web

--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -1,10 +1,39 @@
-import React from "react";
-import './Home.css'
+import React, { useEffect, useState } from "react";
+import "./Home.css";
+import home_01 from "../../assets/images/home/home_01.jpg";
+import home_02 from "../../assets/images/home/home_02.jpg";
+import home_03 from "../../assets/images/home/home_03.jpg";
+import home_04 from "../../assets/images/home/home_04.jpg";
+import home_05 from "../../assets/images/home/home_05.jpg";
+import home_06 from "../../assets/images/home/home_06.jpg";
+import home_07 from "../../assets/images/home/home_07.jpg";
+
+const images = [home_01, home_07, home_05, home_02, home_03, home_04, home_06];
 
 const Home = () => {
+  const [currentImageIndex, setCurrentImageIndex] = useState(0);
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      setCurrentImageIndex((prevIndex) => (prevIndex + 1) % images.length);
+    }, 4500);
+
+    return () => clearInterval(interval);
+  }, []);
+
   return (
-    <div>
-      <h1>Welcome to the Home Page</h1>
+    <div className="home">
+      {/* <h1>Welcome to the Home Page</h1> */}
+      {images.map((image, index) => (
+        <img
+          key={index}
+          className={`home_image ${
+            currentImageIndex === index ? "visible" : ""
+          }`}
+          src={image}
+          alt={`home_image_${index + 1}`}
+        />
+      ))}
     </div>
   );
 };
--- src/Pages/Polaris_Online_Store/Polaris_Online_Store.jsx
+++ src/Pages/Polaris_Online_Store/Polaris_Online_Store.jsx
@@ -1,10 +1,54 @@
 import React from "react";
 import "./Polaris_Online_Store.css";
+import polarisImage_01 from "../../assets/images/Polaris_Store_02.jpg";
+import polarisImage_02 from "../../assets/images/Polaris_Store_03.jpg";
+import polarisImage_03 from "../../assets/images/Polaris_Store_04.jpg";
+import polarisImage_04 from "../../assets/images/Polaris_Store_05.jpg";
+import polarisImage_05 from "../../assets/images/Polaris_Store_09.jpg";
+import polarisImage_06 from "../../assets/images/Polaris_Store_10.jpg";
+import polarisImage_07 from "../../assets/images/Polaris_Store_11.jpg";
 
 const Polaris_Online_Store = () => {
   return (
     <div className="polaris_container">
-      <img src="../../images/Polaris_Store_02.jpg" alt="polaris_image_01" />
+      <div className="polaris_content_01">
+        <h1>Polaris Online Store</h1>
+        <p>
+          About the brand: Polaris is a dynamic online store offering top-tier
+          dropshipping services. Our platform connects consumers with a wide
+          range of products, streamlining the shopping experience and making
+          high-quality items accessible at competitive prices. By utilizing
+          cutting-edge technology, Polaris ensures a seamless and efficient
+          shopping process, catering to the needs of both customers and partner
+          businesses. We focus on enhancing the convenience and satisfaction of
+          our users, driving the future of online retail.
+          <hr />
+          <br />
+          Year — 2024 | Client — Polaris Online Store | Project → Logo & Brand
+          Identity | All rights reserved ©
+        </p>
+      </div>
+      <img className="photo_01" src={polarisImage_01} alt="polaris_image_02" />
+      <img className="photo_02" src={polarisImage_02} alt="polaris_image_03" />
+      <div className="polaris_content_02">
+        <p>
+          The logo incorporates the interaction between the delivery guy, the
+          customer (package receiver) and the “pin” symbol representing the
+          location of the delivery, to cleverly represent the brand's focus on
+          offering top-tier dropshipping services. It's a subtle yet impactful
+          touch. This gives the entire logo a unique, friendly, traditional, and
+          modern look, all of which align with the brand's sustainability
+          message. Overall the logo effectively communicates the brand's
+          commitment to connect consumers with a wide range of products without
+          being overly explicit. It strikes a balance between professionalism
+          and efficiency, making it suitable for a wide range of audiences.
+        </p>
+      </div>
+      <img className="photo_03" src={polarisImage_03} alt="polaris_image_04" />
+      <img className="photo_04" src={polarisImage_04} alt="polaris_image_05" />
+      <img className="photo_05" src={polarisImage_05} alt="polaris_image_09" />
+      <img className="photo_06" src={polarisImage_06} alt="polaris_image_10" />
+      <img className="photo_07" src={polarisImage_07} alt="polaris_image_11" />
     </div>
   );
 };
